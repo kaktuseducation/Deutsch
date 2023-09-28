534cc37cbac49160d700334abb82b1aa6051d173
kaktus education <130348932+kaktuseducation@users.noreply.github.com>
2023-09-28 22:39:44 +0500

    Update size.js

--- js/size.js
+++ js/size.js
@@ -2,7 +2,6 @@ function set_fontsize(){
     var buttons = document.querySelectorAll('.btn');
     var height = buttons[0].offsetHeight;
     var size = Math.floor(height * 0.6) + 'px';
-    console.log(size)
     buttons.forEach(function (button) {
         button.style.fontSize = size;
     });
