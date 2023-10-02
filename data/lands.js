81fc24f79e9c0e6b9f7c5bcd377457c5ef3bc8bb
kaktus education <130348932+kaktuseducation@users.noreply.github.com>
2023-10-03 00:09:38 +0500

    Create lands.js

---
+++ data/lands.js
@@ -0,0 +1,26 @@
+LANDS = {
+  'Baden-Württemberg' : 'In Bearbeitung',
+  'Bayern' : 
+    `Bayern ist das flächengrößte der 16 Länder Deutschlands. Bayern ist das Bundesland im Südosten von Deutschland und seine Hauptstadt ist München. 
+    Der höchste Berg von Bayern und gleichzeitig von ganz Deutschland ist die 2.962 Meter hohe Zugspitze. Außerdem ist Bayern das einzige Bundesland Deutschlands 
+    mit einem Anteil an den Alpen.\n
+    Das Land hat eine reiche Geschichte und viele Sehenswürdigkeiten, die ein gewünsсhtes Reiseziel für Touristen aus der ganzen Welt sind. Die Frauenkirche, 
+    der englische Garten, der Marienplatz mit dem „Alten Peter“ dahinter, Schloss Neuschwanstein sind ein paar davon.\n
+    Bayern wird traditionell als die Heimat der Volksmusik angesehen. Aus Bayern stammen bekannte Komponisten wie Max Reger, Wilfried Hiller, Richard Strauss und 
+    Christoph Willibald Gluck. Volksfeste sind in Bayern weit verbreitet.
+    `,
+  'Berlin' : 'In Bearbeitung',
+  'Brandenburg' : 'In Bearbeitung',
+  'Bremen' : 'In Bearbeitung',
+  'Hamburg' : 'In Bearbeitung',
+  'Hessen' : 'In Bearbeitung',
+  'Mecklenburg-Vorpommern' : 'In Bearbeitung',
+  'Niedersachsen' : 'In Bearbeitung',
+  'Nordrhein-Westfalen' : 'In Bearbeitung',
+  'Rheinland-Pfalz' : 'In Bearbeitung',
+  'Saarland' : 'In Bearbeitung',
+  'Sachsen' : 'In Bearbeitung',
+  'Sachsen-Anhalt' : 'In Bearbeitung',
+  'Schleswig-Holstein' : 'In Bearbeitung',
+  'Thüringen' : 'In Bearbeitung',
+}
