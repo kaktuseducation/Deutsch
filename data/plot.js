85197bbb3e9232f2862d0e24ff776eea41958954
kaktus education <130348932+kaktuseducation@users.noreply.github.com>
2023-10-02 13:18:35 +0500

    Create plot.js

---
+++ data/plot.js
@@ -0,0 +1,12 @@
+PLOT = {
+  1: [
+      {text: 'a', img: 'a.png', position: 'left'},
+      {text: 'b', img: 'b.png', position: 'right'},
+    ],
+  2: [
+      {text: 'a', img: 'a.png', position: 'left'},
+      {text: 'b', img: 'b.png', position: 'right'},
+    ] 
+}
+
+//PLOT[level][step]
