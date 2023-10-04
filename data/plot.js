05de3c2b40d1d996d223d11d7d78f89b42ec08af
kaktus education <130348932+kaktuseducation@users.noreply.github.com>
2023-10-04 23:36:07 +0500

    Update plot.js

--- data/plot.js
+++ data/plot.js
@@ -8,17 +8,17 @@ PLOT = {
       {text: 'Eines Tages unternahm Thomas einen Ausflug zu historischen Seiten Deutschlands. Mit einem erfahrenen Guide zusammen drang er ins Innere der Bibliothek vor, wo jedes Buch wie das Tor zu einer Welt voller fantastischer Abenteuer schien. ', 
        img: 'img/lvl1/back.png', position: 'middle'},
     
-      {text: 'Среди безмерных рядов старинных книг, была одна особенная - старейшая из них, с переплетом из коричневой кожи. Внутри этой книги прятались не только легенды и предания. В ней, словно в сокровищнице, хранились древние артефакты, представляющие каждую федеральную землю. ', 
-       img: 'img/lvl1/book.png', position: 'right'},
+      {text: 'Unter den Reihen alter Bücher war ein ganz besonderes Buch - das älteste davon, mit einem braunen Lederband. In diesem Buch verbargen sich nicht nur Sagen und Legenden. Darin wurden, wie in einer Schatzkammer, alte Artefakte aufbewahrt, die jedes Bundesland Deutschlands darstellten. ', 
+       img: 'img/lvl1/book.png', position: 'middle'},
     
-      {text: 'Однако, судьба юного Томаса приняла необычный оборот. В момент, когда все ушли в следующий зал, Томас, под влечением неведомых сил, незаметно взял эту волшебную книгу. Он собирался спрятаться за высоким стеллажом и изучить её, но споткнулся, и книга из его рук упала на пол.', 
-       img: 'img/lvl1/chips.jpg', position: 'left'},
+      {text: 'Das Schicksal des jungen Thomas nahm jedoch eine ungewöhnliche Wendung. Als alle Besucher in den nächsten Raum gingen, nahm Thomas aus dem Regal dieses magische Buch heraus. Er wollte es durchblättern, aber stolperte zufällig, und das Buch fiel aus seinen Händen zu Boden.', 
+       img: 'img/lvl1/chips.jpg', position: 'middle'},
       
-      {text: 'Вдруг поднялся сильный ветер и страницы древней книги начали быстро перелистываться. В этот момент Томас оказался внутри книги, в самом начале невероятного путешествия. Теперь ему предстоит пройти через каждую землю и собрать древние артефакты, чтобы найти путь обратно в реальный мир.', 
-       img: 'img/lvl1/crocus.jpg', position: 'left'},
+      {text: 'Plötzlich wurde es windig, die Seiten des alten Buches wurden vom starken Wind umgeblättert. In dem Moment gelangte Thomas ins Buch, ganz an den Anfang seiner wundervollen Reise. Jetzt muss er durch jedes Bundesland gehen und alte Artefakte sammeln, um einen Weg zurück in seine reale Welt zu finden.', 
+       img: 'img/lvl1/crocus.jpg', position: 'middle'},
     
-      {text: 'Волшебная книга привела Томаса в прекрасный Нюрнберг прямо в мастерскую известного художника Альбрехта Дюрера. Вместе с Томасом в мастерскую проник и ветер, из-за которого на одну из работ художника пролились краски. Теперь перед нашим героем стоит задача убрать все лишние предметы и вернуть картине первозданный вид.', 
-       img: 'img/lvl1/smile.gif', position: 'right'},
+      {text: 'Das magische Buch führte Thomas in Nürnberg direkt in die Werkstatt des berühmten Künstlers Albrecht Dürer. Zusammen mit Thomas drang auch der Wind in die Werkstatt ein, der die Farben auf dem Gemälde des Künstlers verschüttete. Jetzt steht unser Held vor der Aufgabe, alle unnötigen Gegenstände wegzuräumen und ein ursprüngliches Aussehen dem Gemälde zurückzugeben.', 
+       img: 'img/lvl1/smile.gif', position: 'middle'},
     ],
   2: [
       {text: 'a', img: 'a.png', position: 'left'},
